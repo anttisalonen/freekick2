d853d1b518a11976dae36e067a79602f56f200b7
Antti Salonen <ajsalonen@gmail.com>
2010-04-14 06:55:31 +0200

    add drawing of all pitch lines

--- Match.hs
+++ Match.hs
@@ -126,7 +126,7 @@ loadTexture :: FilePath -> IO TextureObject
 loadTexture fp = do
   eimg <- loadPNGFile fp
   case eimg of
-    Left err  -> throwIO $ mkIOError doesNotExistErrorType ("loading texture:" ++ err) Nothing (Just fp)
+    Left err  -> throwIO $ mkIOError doesNotExistErrorType ("could not load texture: " ++ err) Nothing (Just fp)
     Right img -> do
       let (imgw, imgh) = dimensions img
       texName <- liftM head (genObjectNames 1)
@@ -155,7 +155,7 @@ setCamera' c = do
 playMatch :: Font -> (SWOSTeam, TeamOwner) -> (SWOSTeam, TeamOwner) -> IO ()
 playMatch f _ _ = do
   tex <- loadTexture "grass1.png"
-  evalStateT runMatch (initMatchState tex (16, 16) (70, 100) (20, 40))
+  evalStateT runMatch (initMatchState tex (16, 16) (68, 105) (20, 40))
   putStrLn "Match played! Yay!"
   (w, h) <- liftIO $ getWindowSize
   setCamera ((0, 0), (w, h))
@@ -228,19 +228,63 @@ drawRect r d' = preservingMatrix $ do
 lw :: Float -- linewidth
 lw = 0.15
 
+drawRectBox :: Rectangle -> Float -> Float -> IO ()
+drawRectBox ((p, q), (r, s)) w d = do
+  drawRect ((p,     q),         (r, w)) d
+  drawRect ((p,     q),         (w, s)) d
+  drawRect ((p,     q + s - w), (r, w)) d
+  drawRect ((p + r - w, q),     (w, s)) d
+
+draw2DArc :: (Float, Float) -> Float -> Float -> Float -> IO ()
+draw2DArc p r w d = draw2DArcAngled' p r w d Nothing
+
+draw2DArcAngled :: (Float, Float) -> Float -> Float -> Float -> (Float, Float) -> IO ()
+draw2DArcAngled p r w d as = draw2DArcAngled' p r w d (Just as)
+
+draw2DArcAngled' :: (Float, Float) -> Float -> Float -> Float -> (Maybe (Float, Float)) -> IO ()
+draw2DArcAngled' (xp', yp') r' w' d' an = preservingMatrix $ do
+  let (xp, yp) = (realToFrac xp', realToFrac yp')
+      r = realToFrac r'
+      w = realToFrac w'
+      d = realToFrac d'
+  translate $ Vector3 xp yp (d :: GLfloat)
+  case an of
+    Nothing       -> renderQuadric (QuadricStyle Nothing NoTextureCoordinates Inside FillStyle) (Disk r (r + w) 64 1)
+    Just (a1, a2) -> renderQuadric (QuadricStyle Nothing NoTextureCoordinates Inside FillStyle) (PartialDisk r (r + w) 64 1 (realToFrac a1) (realToFrac a2))
+
+drawSpot :: (Float, Float) -> Float -> Float -> IO ()
+drawSpot p = draw2DArc p 0
+
 drawPitch :: GrassTexture -> FRange -> IO ()
 drawPitch grtexture psize@(px, py) = do
+  loadIdentity
   drawTiling (grasstexobj grtexture) (return ()) ((0, 0), psize) (-1) (grasstiling grtexture)
   color $ Color3 0.8 0.8 (0.8 :: GLfloat)
-  drawRect ((0, 0), (px, lw)) 0
-  drawRect ((0, 0), (lw, py)) 0
-  drawRect ((0, py - lw), (px, lw)) 0
-  drawRect ((px - lw, 0), (lw, py)) 0
-  drawRect ((0, (py - lw) / 2), (px, lw)) 0
-  preservingMatrix $ do
-    translate $ Vector3 (realToFrac px / 2) (realToFrac py / 2) (0 :: GLfloat)
-    renderQuadric (QuadricStyle Nothing NoTextureCoordinates Inside FillStyle) (Disk (9.15) (9.15 + realToFrac lw) 64 1)
-    renderQuadric (QuadricStyle Nothing NoTextureCoordinates Inside FillStyle) (Disk 0 (realToFrac (lw * 2)) 32 1)
+  drawRectBox ((0, 0), (px, py)) lw 0 -- pitch boundaries
+  drawRect ((0, (py - lw) / 2), (px, lw)) 0  -- middle line
+  draw2DArc ((px / 2), (py / 2)) 9.15 lw 0 -- centre ring
+  drawSpot ((px / 2), (py / 2)) (lw * 2) 0 -- centre spot
+  drawRectBox ((px / 2 - 20.16, 0), (40.32, 16.5)) lw 0 -- penalty area 1
+  drawRectBox ((px / 2 - 9.15,  0), (18.3,  5.5))  lw 0 -- goal area 1
+  drawSpot (px / 2, 11) (lw * 2) 0 -- penalty spot 1
+  -- draw2DArcAngled (px / 2, 11) 9.15 lw 0 (-33.367012, 66.7340259) -- penalty arc 1
+  draw2DArcAngled (px / 2, 11.1) 9.15 lw 0 (-54.63298, 109.26596) -- penalty arc 1
+  drawRectBox ((px / 2 - 20.16, py), (40.32, -16.5)) lw 0 -- penalty area 2
+  drawRectBox ((px / 2 - 9.15,  py), (18.3,  -5.5))  lw 0 -- goal area 2
+  drawSpot (px / 2, py - 11) (lw * 2) 0 -- penalty spot 2
+  draw2DArcAngled (px / 2, py - 11.2) 9.15 lw 0 (125.36702, 109.26596) -- penalty arc 2
+  draw2DArcAngled (0, 0) 1 lw 0 (0, 90) -- corner line 1
+  draw2DArcAngled (px, 0) 1 lw 0 (0, -90) -- corner line 2
+  draw2DArcAngled (0, py) 1 lw 0 (90, 90) -- corner line 3
+  draw2DArcAngled (px, py) 1 lw 0 (-90, -90) -- corner line 4
+  draw2DArcAngled (0, 0) 5 lw 0 (0, 0)
+  draw2DArcAngled (10, 10) 5 lw 0 (0, 30)
+  draw2DArcAngled (20, 20) 5 lw 0 (0, 60)
+  draw2DArcAngled (30, 30) 5 lw 0 (0, 120)
+  draw2DArcAngled (40, 40) 5 lw 0 (45, 30)
+  draw2DArcAngled (50, 50) 5 lw 0 (90, 60)
+  draw2DArcAngled (60, 60) 5 lw 0 (-90, -30)
+  draw2DArcAngled (70, 70) 5 lw 0 (-120, -60)
 
 runMatch :: Match ()
 runMatch = do
