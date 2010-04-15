2323a4f50fa9d475d24a5e5c1814d514b9d472c2
Antti Salonen <ajsalonen@gmail.com>
2010-04-15 22:16:57 +0200

    add initial stupid ai

--- src/Match.hs
+++ src/Match.hs
@@ -29,25 +29,33 @@ data MatchTextureSet = MatchTextureSet {
   , humandrawsize     :: (Int, Int)
   }
 
+data PlPosition = Goalkeeper | Defender | Midfielder | Attacker
+  deriving (Eq)
+
+type PlayerID = (Int, Bool)
+
 data Player = Player {
     plposition :: FRange
   , plimage    :: ImageInfo
   , plposz     :: Float
-  , plnumber   :: Int
+  , playerid   :: PlayerID
+  , plpos      :: PlPosition
   }
 $(deriveMods ''Player)
 
-type PlayerID = (Int, Bool)
 type PlayerMap = M.IntMap Player
+type Formation = M.IntMap FRange
 
 data MatchState = MatchState {
-    pitchlist    :: DisplayList
-  , currkeys     :: [SDLKey]
-  , pitchsize    :: (Float, Float)
-  , campos       :: (Float, Float)
-  , homeplayers  :: PlayerMap
-  , awayplayers  :: PlayerMap
-  , controlledpl :: Maybe PlayerID
+    pitchlist     :: DisplayList
+  , currkeys      :: [SDLKey]
+  , pitchsize     :: (Float, Float)
+  , campos        :: (Float, Float)
+  , homeplayers   :: PlayerMap
+  , awayplayers   :: PlayerMap
+  , homeformation :: Formation
+  , awayformation :: Formation
+  , controlledpl  :: Maybe PlayerID
   }
 $(deriveMods ''MatchState)
 
@@ -74,15 +82,57 @@ goRight :: Float -> FRange -> FRange
 goRight n (x, y) = (x + n, y)
 
 initMatchState :: DisplayList -> FRange -> FRange -> MatchTextureSet -> (Swos.SWOSTeam, Swos.SWOSTeam) -> Maybe PlayerID -> MatchState
-initMatchState plist psize cpos pltexs (ht, at) c = MatchState plist [] psize cpos hps aps c
+initMatchState plist psize cpos pltexs (ht, at) c = MatchState plist [] psize cpos hps aps hf af c
   where hps = createPlayers True pltexs psize ht
         aps = createPlayers False pltexs psize at
+        hf  = createFormation True hps
+        af  = createFormation False aps
+
+playerHome :: Player -> Bool
+playerHome = snd . playerid
+
+playerNumber :: Player -> Int
+playerNumber = fst . playerid
+
+mkGoalkeeperFormation :: Bool -> [Player] -> Formation
+mkGoalkeeperFormation True  pls = M.fromList (zip (map playerNumber pls) (repeat (0.5, 0)))
+mkGoalkeeperFormation False pls = M.fromList (zip (map playerNumber pls) (repeat (0.5, 1)))
+
+mkDefenderFormation :: Bool -> [Player] -> Formation
+mkDefenderFormation = mkGoalkeeperFormation
+
+mkMidfielderFormation :: Bool -> [Player] -> Formation
+mkMidfielderFormation = mkGoalkeeperFormation
+
+mkAttackerFormation :: Bool -> [Player] -> Formation
+mkAttackerFormation = mkGoalkeeperFormation
+
+createFormation :: Bool -> PlayerMap -> Formation
+createFormation home pls' =
+  let pls = M.elems pls'
+      gs = filter (\p -> plpos p == Goalkeeper) pls
+      ds = filter (\p -> plpos p == Defender) pls
+      ms = filter (\p -> plpos p == Midfielder) pls
+      fs = filter (\p -> plpos p == Attacker) pls
+      gmap = mkGoalkeeperFormation home gs
+      dmap = mkDefenderFormation home ds
+      mmap = mkMidfielderFormation home ms
+      fmp = mkAttackerFormation home fs
+  in gmap `M.union` dmap `M.union` mmap `M.union` fmp
+
+swosPositionToPosition :: Swos.SWOSPosition -> PlPosition
+swosPositionToPosition p 
+  | Swos.isGoalkeeper p = Goalkeeper
+  | Swos.isDefender   p = Defender
+  | Swos.isMidfielder p = Midfielder
+  | otherwise           = Attacker
 
 swosPlayerToPlayer :: Bool -> MatchTextureSet -> FRange -> Swos.SWOSPlayer -> Player
 swosPlayerToPlayer home texs (px, py) p = 
-  Player (px - 10, py / 2) (ImageInfo tex size) 1 (Swos.plnumber p)
+  Player (px - 10, py / 2) (ImageInfo tex size) 1 ((Swos.plnumber p), home) npos
     where tex = if home then hometexture texs else awaytexture texs
           size = humandrawsize texs
+          npos = swosPositionToPosition (Swos.plposition p)
 
 createPlayers :: Bool -> MatchTextureSet -> FRange -> Swos.SWOSTeam -> PlayerMap
 createPlayers home texs psize t =
@@ -141,21 +191,74 @@ drawPlayer p = drawSprite (imgtexture (plimage p)) (playerTexRectangle p) (plpos
 frameTime :: Word32 -- milliseconds
 frameTime = 10
 
+drawMatch :: Match ()
+drawMatch = do
+  s <- State.get
+  (w, h) <- liftIO $ getWindowSize
+  liftIO $ do
+    clear [ColorBuffer, DepthBuffer]
+    setCamera' (campos s, (fromIntegral (w `div` 20), fromIntegral (h `div` 20)))
+    callList (pitchlist s)
+    mapM_ drawPlayer (M.elems $ homeplayers s)
+    mapM_ drawPlayer (M.elems $ awayplayers s)
+    glSwapBuffers
+
+aiControlled :: MatchState -> PlayerID -> Bool
+aiControlled s n =
+  case controlledpl s of
+    Nothing -> True
+    Just p  -> n /= p
+
+formationPosition :: MatchState -> Player -> FRange
+formationPosition m pl =
+  let (plnum, plhome) = playerid pl 
+      sourcemap       = if plhome then homeformation m else awayformation m
+  in M.findWithDefault (0.5, 0.5) plnum sourcemap
+
+relToAbs :: MatchState -> FRange -> FRange
+relToAbs m (x, y) =
+  let (px, py) = pitchsize m
+  in (px * x, py * y)
+
+formationPositionAbs :: MatchState -> Player -> FRange
+formationPositionAbs m pl =
+  let rel = formationPosition m pl
+  in relToAbs m rel
+
+goto :: FRange -> Player -> Match ()
+goto (x, y) pl = do
+  let (curx,  cury)  = plposition pl
+      (diffx, diffy) = (curx - x, cury - y)
+      c              = playerid pl
+  if abs diffx < 1 && abs diffy < 1
+    then return ()
+    else 
+      if abs diffx > abs diffy
+        then do
+          if diffx > 0
+            then modify $ modPlayer c $ modPlposition (goRight (-plspeed))
+            else modify $ modPlayer c $ modPlposition (goRight (plspeed))
+        else do
+          if diffy > 0
+            then modify $ modPlayer c $ modPlposition (goUp (-plspeed))
+            else modify $ modPlayer c $ modPlposition (goUp (plspeed))
+
+doAI :: Match ()
+doAI = do
+  s <- State.get
+  forM_ (M.elems (awayplayers s) ++ (M.elems (homeplayers s))) $ \pl -> do
+    when (aiControlled s (playerid pl)) $ do
+      goto (formationPositionAbs s pl) pl
+
 runMatch :: Match ()
 runMatch = do
   t1 <- liftIO $ getCPUTime
-  liftIO $ clear [ColorBuffer, DepthBuffer]
-  s <- State.get
-  (w, h) <- liftIO $ getWindowSize
-  liftIO $ setCamera' (campos s, (fromIntegral (w `div` 20), fromIntegral (h `div` 20)))
-  liftIO $ callList (pitchlist s)
-  liftIO $ mapM_ drawPlayer (M.elems $ homeplayers s)
-  liftIO $ mapM_ drawPlayer (M.elems $ awayplayers s)
-  liftIO $ glSwapBuffers
   quitting <- handleKeyEvents
   if quitting
     then return ()
     else do
+      drawMatch
+      doAI
       t2 <- liftIO $ getCPUTime
       let tdiff = floor $ fromIntegral (t2 - t1) * (1e-9 :: Float)
       when (tdiff < frameTime) $ liftIO $ SDL.delay (frameTime - tdiff)
