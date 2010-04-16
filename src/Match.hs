da0cd00c769013b608e2958dd8a0427e8fdc47fb
Antti Salonen <ajsalonen@gmail.com>
2010-04-16 07:29:28 +0200

    add kickoff positions for midfielders and attackers

--- src/Match.hs
+++ src/Match.hs
@@ -99,18 +99,21 @@ mkGoalkeeperFormation True  pls = M.fromList (zip (map playerNumber pls) (repeat
 mkGoalkeeperFormation False pls = M.fromList (zip (map playerNumber pls) (repeat (0.5, 1)))
 
 mkDefenderFormation :: Bool -> [Player] -> Formation
-mkDefenderFormation home pls =
+mkDefenderFormation = kickoffPositions (0.5, 0.2) defs
+  where defs 1 = []
+        defs 2 = [(0.3, 0.2), (0.7, 0.2)]
+        defs 3 = [(0.25, 0.25), (0.75, 0.25)]
+        defs 4 = [(0.2, 0.25), (0.8, 0.25), (0.4, 0.2), (0.6, 0.2)]
+        defs 5 = [(0.2, 0.25), (0.8, 0.25), (0.4, 0.2), (0.6, 0.2)]
+        defs _ = repeat (0.5, 0.2)
+
+kickoffPositions :: FRange -> (Int -> [FRange]) -> Bool -> [Player] -> Formation
+kickoffPositions cnt others home pls =
   let numpls = length pls
       hasscentre = numpls `mod` 2 == 1
       pairpls = if hasscentre then drop 1 pls else pls
-      cplrpos = (0.5, 0.2)
-      pairposs = case numpls of
-                   1 -> []
-                   2 -> [(0.3, 0.2), (0.7, 0.2)]
-                   3 -> [(0.25, 0.25), (0.75, 0.25)]
-                   4 -> [(0.2, 0.25), (0.8, 0.25), (0.4, 0.2), (0.6, 0.2)]
-                   5 -> [(0.2, 0.25), (0.8, 0.25), (0.4, 0.2), (0.6, 0.2)]
-                   _ -> repeat cplrpos
+      cplrpos = cnt
+      pairposs = others numpls
       plrposs = if hasscentre
                   then cplrpos : take (numpls - 1) pairposs
                   else take numpls pairposs
@@ -121,10 +124,21 @@ flipSide :: FRange -> FRange
 flipSide (x, y) = (1 - x, 1 - y)
 
 mkMidfielderFormation :: Bool -> [Player] -> Formation
-mkMidfielderFormation = mkGoalkeeperFormation
+mkMidfielderFormation = kickoffPositions (0.5, 0.3) mids
+  where mids 1 = []
+        mids 2 = [(0.3, 0.3), (0.7, 0.3)]
+        mids 3 = [(0.25, 0.4), (0.75, 0.4)]
+        mids 4 = [(0.1, 0.45), (0.9, 0.45), (0.4, 0.3), (0.6, 0.3)]
+        mids 5 = [(0.1, 0.45), (0.9, 0.45), (0.4, 0.3), (0.6, 0.3)]
+        mids _ = repeat (0.5, 0.3)
 
 mkAttackerFormation :: Bool -> [Player] -> Formation
-mkAttackerFormation = mkGoalkeeperFormation
+mkAttackerFormation = kickoffPositions (0.5, 0.4) poss
+  where poss 1 = []
+        poss 2 = [(0.3, 0.4), (0.7, 0.4)]
+        poss 3 = [(0.25, 0.45), (0.75, 0.45)]
+        poss 4 = [(0.2, 0.45), (0.8, 0.45), (0.4, 0.4), (0.6, 0.4)]
+        poss _ = repeat (0.5, 0.4)
 
 createFormation :: Bool -> PlayerMap -> Formation
 createFormation home pls' =
