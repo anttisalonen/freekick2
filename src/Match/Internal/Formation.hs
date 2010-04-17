057e9a850bb20b73ef352a7fb2bfd68e14631b84
Antti Salonen <ajsalonen@gmail.com>
2010-04-17 07:39:30 +0200

    add some variance to the default formation

--- src/Match/Internal/Formation.hs
+++ src/Match/Internal/Formation.hs
@@ -10,17 +10,8 @@ import Match.Internal.MatchState
 import Match.Internal.MatchBase
 
 mkGoalkeeperFormation :: Bool -> [Player] -> Formation
-mkGoalkeeperFormation True  pls = M.fromList (zip (map playerNumber pls) (repeat (0.5, 0)))
-mkGoalkeeperFormation False pls = M.fromList (zip (map playerNumber pls) (repeat (0.5, 1)))
-
-mkDefenderFormation :: Bool -> [Player] -> Formation
-mkDefenderFormation = kickoffPositions (0.5, 0.2) defs
-  where defs 1 = []
-        defs 2 = [(0.3, 0.2), (0.7, 0.2)]
-        defs 3 = [(0.25, 0.25), (0.75, 0.25)]
-        defs 4 = [(0.2, 0.25), (0.8, 0.25), (0.4, 0.2), (0.6, 0.2)]
-        defs 5 = [(0.2, 0.25), (0.8, 0.25), (0.4, 0.2), (0.6, 0.2)]
-        defs _ = repeat (0.5, 0.2)
+mkGoalkeeperFormation True  pls = M.fromList (zip (map playerNumber pls) (repeat (0.5, 0.02)))
+mkGoalkeeperFormation False pls = M.fromList (zip (map playerNumber pls) (repeat (0.5, 0.98)))
 
 kickoffPositions :: FRange -> (Int -> [FRange]) -> Bool -> [Player] -> Formation
 kickoffPositions cnt others home pls =
@@ -35,21 +26,30 @@ kickoffPositions cnt others home pls =
       plrposs' = if home then plrposs else map flipSide plrposs
   in M.fromList (zip (map playerNumber pls) (plrposs'))
 
+mkDefenderFormation :: Bool -> [Player] -> Formation
+mkDefenderFormation = kickoffPositions (0.5, 0.2) defs
+  where defs 1 = []
+        defs 2 = [(0.33, 0.24), (0.76, 0.21)]
+        defs 3 = [(0.258, 0.252), (0.753, 0.257)]
+        defs 4 = [(0.22, 0.250), (0.83, 0.250), (0.48, 0.25), (0.60, 0.27)]
+        defs 5 = [(0.23, 0.258), (0.88, 0.257), (0.41, 0.29), (0.62, 0.26)]
+        defs _ = repeat (0.5, 0.2)
+
 mkMidfielderFormation :: Bool -> [Player] -> Formation
 mkMidfielderFormation = kickoffPositions (0.5, 0.3) mids
   where mids 1 = []
-        mids 2 = [(0.3, 0.3), (0.7, 0.3)]
-        mids 3 = [(0.25, 0.4), (0.75, 0.4)]
-        mids 4 = [(0.1, 0.45), (0.9, 0.45), (0.4, 0.3), (0.6, 0.3)]
-        mids 5 = [(0.1, 0.45), (0.9, 0.45), (0.4, 0.3), (0.6, 0.3)]
+        mids 2 = [(0.32, 0.34), (0.76, 0.37)]
+        mids 3 = [(0.25, 0.41), (0.751, 0.46)]
+        mids 4 = [(0.13, 0.452), (0.92, 0.453), (0.41, 0.30), (0.63, 0.38)]
+        mids 5 = [(0.12, 0.450), (0.95, 0.451), (0.46, 0.38), (0.67, 0.39)]
         mids _ = repeat (0.5, 0.3)
 
 mkAttackerFormation :: Bool -> [Player] -> Formation
 mkAttackerFormation = kickoffPositions (0.5, 0.4) poss
   where poss 1 = []
-        poss 2 = [(0.3, 0.4), (0.7, 0.4)]
-        poss 3 = [(0.25, 0.45), (0.75, 0.45)]
-        poss 4 = [(0.2, 0.45), (0.8, 0.45), (0.4, 0.4), (0.6, 0.4)]
+        poss 2 = [(0.31, 0.42), (0.64, 0.44)]
+        poss 3 = [(0.256, 0.452), (0.754, 0.458)]
+        poss 4 = [(0.21, 0.455), (0.82, 0.453), (0.41, 0.42), (0.62, 0.46)]
         poss _ = repeat (0.5, 0.4)
 
 createFormation :: Bool -> PlayerMap -> Formation
@@ -69,8 +69,8 @@ formationPosition :: MatchState -> Player -> FRange
 formationPosition m pl =
   let (kx, ky) = kickoffPosition m pl
   in if playerHome pl
-       then (kx, ky * 2)
-       else (kx, 1 - (1 - ky) * 2)
+       then (kx, ky * 1.5)
+       else (kx, 1 - (1 - ky) * 1.2)
 
 formationPositionAbs :: MatchState -> Player -> FRange
 formationPositionAbs m pl =
