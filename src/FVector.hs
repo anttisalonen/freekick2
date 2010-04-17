cdd0fbdb8366c101a8f0414cd1afeda340245a42
Antti Salonen <ajsalonen@gmail.com>
2010-04-17 05:49:21 +0200

    add some helper functions

--- src/FVector.hs
+++ src/FVector.hs
@@ -23,6 +23,9 @@ setZ v (x, y, _) = (x, y, v)
 (*+*) :: FVector3 -> FVector3 -> FVector3
 (x1, y1, z1) *+* (x2, y2, z2) = (x1 + x2, y1 + y2, z1 + z2)
 
+(*-*) :: FVector3 -> FVector3 -> FVector3
+(x1, y1, z1) *-* (x2, y2, z2) = (x1 - x2, y1 - y2, z1 - z2)
+
 (***) :: FVector3 -> Float -> FVector3
 (x1, y1, z1) *** s = (x1 * s, y1 * s, z1 * s)
 
@@ -32,10 +35,17 @@ to2D (x, y, _) = (x, y)
 nullFVector3 :: FVector3
 nullFVector3 = (0, 0, 0)
 
-dist22 :: FRange -> FRange -> Float
-dist22 (x1, y1) (x2, y2) = ((x2 - x1)**2) + ((y2 - y1)**2)
+dist2squared :: FRange -> FRange -> Float
+dist2squared (x1, y1) (x2, y2) = ((x2 - x1)**2) + ((y2 - y1)**2)
 
 dist2 :: FRange -> FRange -> Float
-dist2 p1 p2 = sqrt $ dist22 p1 p2
+dist2 p1 p2 = sqrt $ dist2squared p1 p2
 
+diff2 :: FRange -> FRange -> FRange
+diff2 (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
 
+mul2 :: FRange -> Float -> FRange
+mul2 (x, y) s = (x * s, y * s)
+
+to3D :: FRange -> Float -> FVector3
+to3D (x, y) z = (x, y, z)
