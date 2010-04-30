8e0014c98b9b451c21e097e9fb5ba494ef746460
Antti Salonen <ajsalonen@gmail.com>
2010-04-30 22:26:33 +0200

    add helper functions

--- src/Utils.hs
+++ src/Utils.hs
@@ -3,3 +3,15 @@ where
 
 clamp mn mx v = min mx $ max mn v
 
+radToDeg :: (Floating a) => a -> a
+radToDeg = (/pi) . (*180)
+
+wrap :: (Num a, Ord a) => a -> a -> a -> a
+wrap mn mx v =
+  if v < mn
+    then wrap mn mx (v + diff)
+    else if v > mx
+           then wrap mn mx (v - diff)
+           else v
+    where diff = mx - mn
+
